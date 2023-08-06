82dd6a98d46442dc1bd100d91c0f67a4bf638b4d
WassimZo <wassdzouaoui@gmail.com>
2023-08-06 16:39:13 +0200

    mise en place redux

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,5 +1,9 @@
 import CodeTab from "./CodeTab"
+import { useSelector } from "react-redux"
 export default function Tabs() {
+
+  const tabs = useSelector(state => state.tabs);
+  
   return (
     <div className="flex grow">
         <div className="grow flex flex-col w-[175px] shrink-0 text-slate-300 border-r border-slate-200">
---
+++ src/features/tabs.js
@@ -0,0 +1,51 @@
+import html from '../assets/html.svg'
+import css from '../assets/css.svg'
+import js from '../assets/js.svg'
+
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = [
+    {
+        id: 1,
+        lang: 'html',
+        imgUrl: html,
+        buttonContent: "HTML",
+        code: `<div>
+        <h1>Editeur de code avec React </h1>
+        <p>Codez directement sur votre navigateur. </p>
+        </div>`
+    },
+    {
+        id: 2,
+        lang: 'css',
+        imgUrl: css,
+        buttonContent: "CSS",
+        code: `body {
+            font-family: Roboto, sans-serif;
+            padding: 25px;
+            color: #111;
+            background-color: #f1f1f1;
+        }
+        `
+    },
+    {
+        id: 3,
+        lang: 'javascript',
+        imgUrl: js,
+        buttonContent: "JavaScript",
+        code: `console.log("Hello World !")`
+    }
+]
+
+export const codeUpdater = createSlice({
+    name: "code-updater",
+    initialState,
+    reducers: {
+        updateCode: (state, action) => {
+            state.find(obj => obj.id == action.payload.id).code = action.payload.value;
+        }
+    }
+})
+
+export const {updateCode} = codeUpdater.actions
+export default codeUpdater.reducer
